fd136763118eb9cc4a153a7eae9e9cc45df57f03
Readpato <87768087+Readpato@users.noreply.github.com>
2022-02-14 11:37:09 +0100

    chore: Create components for app
    The components that compose the application have been created with placeholder content

---
+++ random-user-generator/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import MainContent from "./components/MainContent.vue";
+
+export default {
+  components: {
+    MainContent,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <MainContent />
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.container {
+  width: 80%;
+  height: auto;
+  margin: 0 auto;
+  display: block;
+  border: 2px solid red;
+}
+</style>
---
+++ random-user-generator/src/components/UserDescription.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "UserDescription",
+  data() {
+    return {
+      firstName: "Patrick",
+      lastName: "Raedler",
+      location: "Florence",
+      email: "patoraedler@gmail.com",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="user-description">
+    <img src="/" :alt="`Image of ${firstName} ${lastName}`" />
+    <h2>{{ firstName }} {{ lastName }}</h2>
+    <p>{{ location }}</p>
+    <p>{{ email }}</p>
+  </div>
+</template>
+
+<style scoped></style>
